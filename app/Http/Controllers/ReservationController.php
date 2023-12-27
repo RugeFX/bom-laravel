2f11e87863332f95d5a705fd41b81f2a8dda29cb
banu1312 <radityabanu1312@gmail.com>
2023-12-27 23:39:13 +0800

    fixing bugs

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -48,10 +48,10 @@ class ReservationController extends Controller
                 "pickupPlan_code" => "required|string|exists:plans,plan_code",
                 "reservation_code" => "required|string|unique:reservations,reservation_code",
                 'helmet' => 'required|array',
-                'fak' => 'array',
+                'fak' => 'required|array',
                 'motor' => 'required|array',
                 'hardcase' => 'array',
-                "fak.*.fak_code" => "string|exists:fakItems,code",
+                "fak.*.fak_code" => "required|string|exists:fakItems,code",
                 "helmet.*.helmet_code"=>"required|string|exists:helmetItems,code",
                 "motor.*.motor_code"=>"required|string|exists:motorItems,code",
                 "hardcase.*.hardcase_code"=>"string|exists:hardcaseItems,code",
@@ -62,29 +62,38 @@ class ReservationController extends Controller
                 ],
                 'information'=>"string",
             ]);
-            $fak = convert_array($validated["fak"]);
-            $helmet = convert_array($validated["helmet"]);
-            $motor = convert_array($validated["motor"]);
-            $hardcase = convert_array($validated["hardcase"]);
             $data = Reservation::query()->create($validated);
-            foreach ($validated['motor'] as $piece) {
-                if ($piece["motor_code"] ?? false) {
-                    $data->motorItems()->sync($motor);
+            if(array_key_exists("fak",$validated)){
+                $fak = convert_array($validated["fak"]);
+                foreach ($validated['fak'] as $piece) {
+                    if ($piece["fak_code"] ?? false) {
+                        $data->fakItems()->sync($fak);
+                    }
+                }
+
+            }
+            if(array_key_exists("helmet",$validated)){
+                $helmet = convert_array($validated["helmet"]);
+                foreach ($validated['helmet'] as $piece) {
+                    if ($piece["helmet_code"] ?? false) {
+                        $data->helmetItems()->sync($helmet);
+                    }
                 }
             }
-            foreach ($validated['helmet'] as $piece) {
-                if ($piece["helmet_code"] ?? false) {
-                    $data->helmetItems()->sync($helmet);
+            if(array_key_exists("motor",$validated)){
+                $motor = convert_array($validated["motor"]);
+                foreach ($validated['motor'] as $piece) {
+                    if ($piece["motor_code"] ?? false) {
+                        $data->motorItems()->sync($motor);
+                    }
                 }
             }
-            foreach ($validated['fak'] as $piece) {
-                if ($piece["fak_code"] ?? false) {
-                    $data->fakItems()->sync($fak);
-                }
-            }
-            foreach ($validated['hardcase'] as $piece) {
-                if ($piece["hardcase_code"] ?? false) {
-                    $data->hardcaseItems()->sync($hardcase);
+            if(array_key_exists("hardcase",$validated)){
+                $hardcase = convert_array($validated["hardcase"]);
+                foreach ($validated['hardcase'] as $piece) {
+                    if ($piece["hardcase_code"] ?? false) {
+                        $data->hardcaseItems()->sync($hardcase);
+                    }
                 }
             }
             $data->update($validated);
@@ -134,14 +143,14 @@ class ReservationController extends Controller
             $validated = $request->validate([
                 "returnPlan_code" => "string|exists:plans,plan_code",
                 "reservation_code" => ["string", \Illuminate\Validation\Rule::unique('reservations', 'reservation_code')->ignore($reservation->reservation_code, "reservation_code")],
-                'helmet' => 'array',
-                'fak' => 'array',
-                'motor' => 'array',
+                'helmet' => 'required|array',
+                'fak' => 'required|array',
+                'motor' => 'required|array',
                 'hardcase' => 'array',
-                "fak.*.fak_code" => "string|exists:fakItems,code",
+                "fak.*.fak_code" => "required|string|exists:fakItems,code",
                 "hardcase.*.hardcase_code" => "string|exists:hardcaseItems,code",
-                "helmet.*.helmet_code"=>"string|exists:helmetItems,code",
-                "motor.*.motor_code"=>"string|exists:motorItems,code",
+                "helmet.*.helmet_code"=>"required|string|exists:helmetItems,code",
+                "motor.*.motor_code"=>"required|string|exists:motorItems,code",
                 "fak.*.status" =>[
                     'string',
                     Rule::in(['Complete','Incomplete']),
@@ -164,28 +173,37 @@ class ReservationController extends Controller
                 ],
                 'information'=>"string",
             ]);
-            $fak = convert_array($validated["fak"]);
-            $helmet = convert_array($validated["helmet"]);
-            $motor = convert_array($validated["motor"]);
-            $hardcase = convert_array($validated["hardcase"]);
-            foreach ($validated['motor'] as $piece) {
-                if ($piece["motor_code"] ?? false) {
-                    $reservation->motorItems()->sync($motor);
+            if(array_key_exists("fak",$validated)){
+                $fak = convert_array($validated["fak"]);
+                foreach ($validated['fak'] as $piece) {
+                    if ($piece["fak_code"] ?? false) {
+                        $reservation->fakItems()->sync($fak);
+                    }
+                }
+
+            }
+            if(array_key_exists("helmet",$validated)){
+                $helmet = convert_array($validated["helmet"]);
+                foreach ($validated['helmet'] as $piece) {
+                    if ($piece["helmet_code"] ?? false) {
+                        $reservation->helmetItems()->sync($helmet);
+                    }
                 }
             }
-            foreach ($validated['helmet'] as $piece) {
-                if ($piece["helmet_code"] ?? false) {
-                    $reservation->helmetItems()->sync($helmet);
+            if(array_key_exists("motor",$validated)){
+                $motor = convert_array($validated["motor"]);
+                foreach ($validated['motor'] as $piece) {
+                    if ($piece["motor_code"] ?? false) {
+                        $reservation->motorItems()->sync($motor);
+                    }
                 }
             }
-            foreach ($validated['fak'] as $piece) {
-                if ($piece["fak_code"] ?? false) {
-                    $reservation->fakItems()->sync($fak);
-                }
-            }
-            foreach ($validated['hardcase'] as $piece) {
-                if ($piece["hardcase_code"] ?? false) {
-                    $reservation->hardcaseItems()->sync($hardcase);
+            if(array_key_exists("hardcase",$validated)){
+                $hardcase = convert_array($validated["hardcase"]);
+                foreach ($validated['hardcase'] as $piece) {
+                    if ($piece["hardcase_code"] ?? false) {
+                        $reservation->hardcaseItems()->sync($hardcase);
+                    }
                 }
             }
             $reservation->update($validated);
