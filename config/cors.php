b8762a02492a93899f309ca709e39ae372731638
rugefx <zackfxg@gmail.com>
2023-12-29 18:01:05 +0700

    deployment test

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    'allowed_origins' => ['https://bom.rugefx.com'],
 
     'allowed_origins_patterns' => [],
 
--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,127.0.0.1:5174,localhost:5174,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,127.0.0.1:5174,localhost:5174,bom.rugefx.com,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
